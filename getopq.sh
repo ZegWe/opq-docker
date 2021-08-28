1aa2a3f197a96a608f59aaa82b3e357ddf77069b
ZegWe <abdccdba147@gmail.com>
2021-08-28 23:15:50 +0800

    upgrade to 6.0.20

--- getopq.sh
+++ getopq.sh
@@ -6,19 +6,19 @@ apk add --no-cache curl tar
 
 if [[ "$platform" = "linux/amd64" ]]
 then
-    curl -O https://files.gitter.im/5f27939ed73408ce4feb3112/zMQB/OPQBot_6.0.18_linux_amd64.tar.gz
-    tar -xvf OPQBot_6.0.18_linux_amd64.tar.gz
-    mv OPQBot_6.0.18_linux_amd64 OPQBot
+    curl -O https://github.com/opq-osc/OPQ/releases/download/v6.0.20/OPQBot_6.0.20_linux_amd64.tar.gz
+    tar -xvf OPQBot_6.0.20_linux_amd64.tar.gz
+    mv OPQBot_6.0.20_linux_amd64 OPQBot
 elif [[ "$platform" = "linux/arm/v7" ]]
 then
-    curl -O https://files.gitter.im/5f27939ed73408ce4feb3112/7FDm/OPQBot_6.0.18_linux_arm.tar.gz
-    tar -xvf OPQBot_6.0.18_linux_arm.tar.gz
-    mv OPQBot_6.0.18_linux_arm OPQBot
+    curl -O https://github.com/opq-osc/OPQ/releases/download/v6.0.20/OPQBot_6.0.20_linux_arm.tar.gz
+    tar -xvf OPQBot_6.0.20_linux_arm.tar.gz
+    mv OPQBot_6.0.20_linux_arm OPQBot
 elif [[ "$platform" = "linux/arm64" ]]
 then
-    curl -O https://files.gitter.im/5f27939ed73408ce4feb3112/7FDm/OPQBot_6.0.18_linux_arm64.tar.gz
-    tar -xvf OPQBot_6.0.18_linux_arm64.tar.gz
-    mv OPQBot_6.0.18_linux_arm64 OPQBot
+    curl -O https://github.com/opq-osc/OPQ/releases/download/v6.0.20/OPQBot_6.0.20_linux_arm64.tar.gz
+    tar -xvf OPQBot_6.0.20_linux_arm64.tar.gz
+    mv OPQBot_6.0.20_linux_arm64 OPQBot
 else
     echo "Platform Not Available !"
 fi
