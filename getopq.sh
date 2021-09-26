cb9437c14078854f5c6fcb6a1ec6461522878e7a
Tribunny <45593350+Tribunny@users.noreply.github.com>
2021-09-26 16:44:29 +0800

    Update getopq.sh
    add proxy for download application.

--- getopq.sh
+++ getopq.sh
@@ -6,17 +6,17 @@ apk add --no-cache curl tar
 
 if [[ "$platform" = "linux/amd64" ]]
 then
-    curl -LO https://github.com/opq-osc/OPQ/releases/download/v6.0.20/OPQBot_6.0.20_linux_amd64.tar.gz
+    curl -LO https://ghproxy.com/https://github.com/opq-osc/OPQ/releases/download/v6.0.20/OPQBot_6.0.20_linux_amd64.tar.gz
     tar -xvf OPQBot_6.0.20_linux_amd64.tar.gz
     mv OPQBot_6.0.20_linux_amd64 OPQBot
 elif [[ "$platform" = "linux/arm/v7" ]]
 then
-    curl -LO https://github.com/opq-osc/OPQ/releases/download/v6.0.20/OPQBot_6.0.20_linux_arm.tar.gz
+    curl -LO https://ghproxy.com/https://github.com/opq-osc/OPQ/releases/download/v6.0.20/OPQBot_6.0.20_linux_arm.tar.gz
     tar -xvf OPQBot_6.0.20_linux_arm.tar.gz
     mv OPQBot_6.0.20_linux_arm OPQBot
 elif [[ "$platform" = "linux/arm64" ]]
 then
-    curl -LO https://github.com/opq-osc/OPQ/releases/download/v6.0.20/OPQBot_6.0.20_linux_arm64.tar.gz
+    curl -LO https://ghproxy.com/https://github.com/opq-osc/OPQ/releases/download/v6.0.20/OPQBot_6.0.20_linux_arm64.tar.gz
     tar -xvf OPQBot_6.0.20_linux_arm64.tar.gz
     mv OPQBot_6.0.20_linux_arm64 OPQBot
 else
